da23c4780681bea0efcc468d4d6c2dda457943e4
Robert <junhojeon010@gmail.com>
2024-05-06 22:48:04 -0600

    removed instance of getComputerChoice() and getHumanChoice()

--- main.js
+++ main.js
@@ -3,69 +3,56 @@ computerScore = 0;
 humanChoice = ""
 computerChoice = ""
 
-function playGame(){
-    function getComputerChoice(){
-        let number = Math.random();
-        if ((0 <= number) && (number < 1/3)) {
-            return computerChoice = "rock"; 
-        } 
-        else if ((1/3 <= number) && (number < 2/3)) {
-            return computerChoice = "scissors";
-        } 
-        else if ((2/3 <= number) && (number < 1)) {
-            return computerChoice = "paper";
-        }
-    } getComputerChoice()
-    
-    function getHumanChoice(){
-        entryPrompt = prompt("Enter a choice: ")
-        humanChoice = entryPrompt.toLowerCase()
-        return humanChoice
-    } getHumanChoice()
+function getHumanChoice(){
+    entryPrompt = prompt("Enter rock, paper, or scissors: ")
+    humanChoice = entryPrompt.toLowerCase()
+    return humanChoice
+} 
 
-    function playRound(x, y){
-        if ((humanChoice === "rock") && (computerChoice === "paper")) {
-            computerScore += 1;
-            console.log("Paper beats Rock!");
-        } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
-            humanScore += 1;
-            console.log("Rock beats Scissors!");
-        } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
-            humanScore += 1;
-            console.log("Paper beats Rock!");
-        } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
-            computerScore += 1;
-            console.log("Scissors beats Paper!");
-        } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
-            humanScore += 1;
-            console.log("Scissors beats Paper!");
-        } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
-            computerScore += 1;
-            console.log("Rock beats Scissors!");
-        } else if (humanChoice === computerChoice) {
-            console.log("Tie Game!");
-        } 
-} playRound(humanChoice, computerChoice)}
+function getComputerChoice(){
+    let number = Math.random();
+    if ((0 <= number) && (number < 1/3)) {
+        return computerChoice = "rock"; 
+    } 
+    else if ((1/3 <= number) && (number < 2/3)) {
+        return computerChoice = "scissors";
+    } 
+    else if ((2/3 <= number) && (number < 1)) {
+        return computerChoice = "paper";
+    }
+} 
 
-playGame()
-console.log(humanChoice, computerChoice)
-console.log("User = " + humanScore, "Computer = " + computerScore)
+function playRound(x, y){
+    if ((humanChoice === "rock") && (computerChoice === "paper")) {
+        computerScore += 1;
+        console.log("Paper beats Rock!");
+    } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+        humanScore += 1;
+        console.log("Rock beats Scissors!");
+    } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
+        humanScore += 1;
+        console.log("Paper beats Rock!");
+    } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+        computerScore += 1;
+        console.log("Scissors beats Paper!");
+    } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+        humanScore += 1;
+        console.log("Scissors beats Paper!");
+    } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+        computerScore += 1;
+        console.log("Rock beats Scissors!");
+    } else if (humanChoice === computerChoice) {
+        console.log("Tie Game!");
+    }}
 
-playGame()
-console.log(humanChoice, computerChoice)
-console.log("User = " + humanScore, "Computer = " + computerScore)
+function playGame(x) {
+    for (i = 0; i < x; i++) {
+        getHumanChoice();
+        getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }}
 
-playGame()
-console.log(humanChoice, computerChoice)
-console.log("User = " + humanScore, "Computer = " + computerScore)
-
-playGame()
-console.log(humanChoice, computerChoice)
-console.log("User = " + humanScore, "Computer = " + computerScore)
-
-playGame()
-console.log(humanChoice, computerChoice)
-console.log("User = " + humanScore, "Computer = " + computerScore)
+playGame(5)
 
 function finalTally(humanScore, computerScore) {
     if (humanScore > computerScore) {
@@ -75,5 +62,4 @@ function finalTally(humanScore, computerScore) {
     } else if (computerScore === humanScore) {
         console.log("It is a tie!");
     }
-}
-finalTally(humanScore, computerScore)
+} finalTally(humanScore, computerScore)
